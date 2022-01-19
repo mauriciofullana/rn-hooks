f4a00fc86a4c1b6c6340afc2155a4366f63e9ca9
Mauricio Fullana <mfullana4@gmail.com>
2022-01-18 21:30:38 -0300

    useRef basic implementation

--- app/screens/UseRef.tsx
+++ app/screens/UseRef.tsx
@@ -1,10 +1,26 @@
-import React, { FunctionComponent, useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { FunctionComponent, useRef, useState } from "react";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 const UseRefScreen: FunctionComponent = () => {
+  const [name, setName] = useState<string>("Mauricio");
+  const inputRef = useRef<TextInput>(null);
+
+  const handleButtonPress = () => {
+    inputRef.current?.focus();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>useRef</Text>
+      <Text style={styles.text}>{name}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          ref={inputRef}
+          value={name}
+          onChangeText={setName}
+          placeholder="Name"
+        />
+      </View>
+      <Button title="Focus" onPress={handleButtonPress} />
     </View>
   );
 };
@@ -16,5 +32,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 25,
+  },
+  text: {
+    fontSize: 32,
+  },
+  inputContainer: {
+    width: "100%",
+    borderColor: "black",
+    borderBottomWidth: 1,
+    marginTop: 20,
+    paddingVertical: 10,
   },
 });
