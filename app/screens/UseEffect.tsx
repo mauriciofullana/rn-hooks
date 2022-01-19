017a9e3028024f7c2400619a6b377aa3cc617e44
Mauricio Fullana <mfullana4@gmail.com>
2022-01-18 21:01:42 -0300

    useEffect basic implementation

--- app/screens/UseEffect.tsx
+++ app/screens/UseEffect.tsx
@@ -1,10 +1,26 @@
-import React, { FunctionComponent, useState } from "react";
+import axios from "axios";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const UseEffectScreen: FunctionComponent = () => {
+  const [dependecy, setDependecy] = useState<boolean>(false);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((res) => console.log(res.data));
+    return () => {
+      console.log("clean up");
+    };
+  }, [dependecy]);
+
+  const handleState = () => {
+    setDependecy((prev) => !prev);
+  };
+
   return (
     <View style={styles.container}>
       <Text>useEffect</Text>
+      <Button title="Change state" onPress={handleState} />
     </View>
   );
 };
